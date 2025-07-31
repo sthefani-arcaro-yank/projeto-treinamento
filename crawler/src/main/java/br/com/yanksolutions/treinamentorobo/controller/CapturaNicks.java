2bd317d851cba3e3903c4f2e0a1eab4825aea231
unknown <sthefaniarcaro@yanksolutions.com.br>
2025-07-31 11:34:03 -0300

    feat: adiciona botao para gerar nicks

--- crawler/src/main/java/br/com/yanksolutions/treinamentorobo/controller/CapturaNicks.java
+++ crawler/src/main/java/br/com/yanksolutions/treinamentorobo/controller/CapturaNicks.java
@@ -20,13 +20,21 @@ public class CapturaNicks {
     }
 
     @After
-    public void afterEach(){
+    public void afterEach() throws InterruptedException {
+        Thread.sleep(10000);
         this.capturaNicks.fecharPagina();
     }
 
     @Test
-    public void gerarNicks(){
+    public void gerarNicks() throws InterruptedException {
         capturaNicks.acessarGerador("//*[@id=\"top-nav\"]/li[9]/a");
+        Thread.sleep(5000);
         capturaNicks.selecionarMetodo("//*[@id=\"method\"]", "Aleatório");
+        Thread.sleep(5000);
+        capturaNicks.selecionarQuantidade("//*[@id=\"quantity\"]", 50);
+        Thread.sleep(5000);
+        capturaNicks.selecionarMetodo("//*[@id=\"limit\"]", "8");
+        Thread.sleep(5000);
+        capturaNicks.submiteBotao("//*[@id=\"bt_gerar_nick\"]");
     }
 }
--- crawler/src/main/java/br/com/yanksolutions/treinamentorobo/controller/pages/CapturaNicksPage.java
+++ crawler/src/main/java/br/com/yanksolutions/treinamentorobo/controller/pages/CapturaNicksPage.java
@@ -5,11 +5,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class CapturaNicksPage {
     private WebDriver browser;
     private static final String URL = "https://www.4devs.com.br/gerador_de_texto_lorem_ipsum";
+
+    private WebElement esperar(String xpath) {
+        return new WebDriverWait(browser, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+    }
+
 
     public CapturaNicksPage(){
         System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
@@ -30,8 +40,21 @@ public class CapturaNicksPage {
     }
 
     public void selecionarMetodo(String xpathSelect, String textoOpcao){
+
         WebElement elementoSelect = browser.findElement(By.xpath(xpathSelect));
         Select select = new Select(elementoSelect);
         select.selectByVisibleText(textoOpcao);
     }
+
+    public void selecionarQuantidade(String elemento, int quantidade){
+        WebElement campo =  browser.findElement(By.xpath(elemento));
+        campo.clear();
+        campo.sendKeys(String.valueOf(quantidade));
+    }
+
+    public void submiteBotao(String elemento){
+        browser.findElement(By.xpath(elemento)).click();
+    }
+
+
 }
